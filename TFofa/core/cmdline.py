48318e7eedb67118229c2553900278827d9a21a8
Ethancck <46082627+Ethancck@users.noreply.github.com>
2021-10-11 18:44:50 +0800

    Add files via upload
    功能更新

--- TFofa/core/cmdline.py
+++ TFofa/core/cmdline.py
@@ -5,10 +5,12 @@ parser = argparse.ArgumentParser(prog='Fofa', usage=usage)
 report = parser.add_argument_group("Report", "Report options")
 report.add_argument('--out', dest="report_name", type=str, default="result.csv",
                     help="generate the report name,default(result.csv)")
-report.add_argument('--cidr', dest="cidr", action="store_true",
+report.add_argument('--cidr', dest="cidr",action="store_true",
                     help="generate cidr file(default cidr.txt)")
 # Requests options
 request = parser.add_argument_group("Request", "request options")
-request.add_argument("--size", dest="size", type=int, default=100, help="The Fofa API Requests of sizes(default 100) ")
+request.add_argument("--size", dest="size", type=int, default=10000, help="The Fofa API Requests of sizes(default 100) ")
 request.add_argument("--query", dest="query", type=str, help="your query words!")
+request.add_argument("--cert", dest="cert", type=str, help="your domains file!(dork:'cert=\"domain\" && type=\"subdomain\" && country=\"CN\"')")
+request.add_argument("--status_code", dest="status_code", type=str, default="",help="dork:status_code=""")
 args = parser.parse_args()
---
+++ TFofa/core/lib.py
@@ -0,0 +1,33 @@
+
+import re
+cidr = [
+ '1.1.1.1', '2.2.2.2']
+
+def ip2cidr(ip):
+    ipset = set()
+    ipset.add(re.findall('\\d+?\\.\\d+?\\.\\d+?\\.', ip)[0] + '0/24')
+    iplists = list(ipset)
+    iplists.sort()
+    return iplists
+
+def readFile(file):
+    ipf = open('./{}'.format(file), 'r')
+    return ipf
+
+def saveContext(s,file):
+    n=0
+    ips=set()
+    wfile = open('./{}'.format(file), 'w')
+    for i in s:
+        ips.add(i[0])
+    for ip in ips:
+        n=n+1
+        wfile.write(ip + '\n')
+    else:
+        wfile.close()
+    return n,ips
+
+if __name__ == '__main__':
+    iplist = ip2cidr(cidr)
+    saveContext(iplist)
+
